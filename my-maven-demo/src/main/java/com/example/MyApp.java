f0c39d73be39f01b7924b064d026c6705911774e
Selbie01 <sawyerselby@gmail.com>
2023-09-28 15:22:53 -0500

    first commit

---
+++ my-maven-demo/src/main/java/com/example/MyApp.java
@@ -0,0 +1,7 @@
+package com.example; 
+import org.apache.commons.lang3.StringUtils; 
+public class MyApp{ 
+public static void main(String[] args) {
+String message = "Hello, Maven!"; 
+System.out.println(StringUtils.chop(message)); } 
+} 
